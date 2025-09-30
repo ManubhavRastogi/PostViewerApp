7bbb1054dbd87d20bac50e76baf00656d4005995
Manubhav Rastogi <manubhav@apptunix.com>
2025-09-30 19:13:40 +0530

    Initial project Setup

---
+++ PostViewerApp/PostViewerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostViewerAppApp.swift
+//  PostViewerApp
+//
+//  Created by Manubhav Rastogi on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PostViewerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
